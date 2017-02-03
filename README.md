18fe64afc9c3389b33bd22970e503b57c52653ba
Curtis Brandt <curtis@cpan.org>
2017-02-03 00:03:06 -0800

    Switch to FreeBSD license.

--- README.md
+++ README.md
@@ -50,10 +50,4 @@ Please report bugs on my Github: [https://github.com/aggrolite/POE-Component-IRC
 
 # LICENSE AND COPYRIGHT
 
-Copyright 2013 Curtis Brandt.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
+Copyright 2013-2017 Curtis Brandt. See LICENSE for details.
--- lib/POE/Component/IRC/Plugin/WWW/Reddit/TIL.pm
+++ lib/POE/Component/IRC/Plugin/WWW/Reddit/TIL.pm
@@ -126,14 +126,6 @@ Please report bugs on my Github: L<https://github.com/aggrolite/POE-Component-IR
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013 Curtis Brandt.
-
-This program is free software; you can redistribute it and/or modify it
-under the terms of either: the GNU General Public License as published
-by the Free Software Foundation; or the Artistic License.
-
-See http://dev.perl.org/licenses/ for more information.
+Copyright 2013-2017 Curtis Brandt. See LICENSE for details.
 
 =cut
-
-1;
